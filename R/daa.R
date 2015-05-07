22bf680c8d9a102698834827c20be693f84f3e48
thiloklein <thilo@klein.co.uk>
2015-05-07 12:14:25 +0200

    bug fix: while loop in daa function

--- R/daa.R
+++ R/daa.R
@@ -23,14 +23,12 @@
 #' or women (in the stable marriage problem) in the market. Defaults to \code{ncol(c.prefs)}.
 #' @param nSlots vector of length \code{nColleges} indicating the number of places (i.e. 
 #' quota) of each college. Defaults to \code{rep(1,nColleges)} for the marriage problem.
-#' @param s.prefs matrix of dimension \code{nColleges} \code{x} \code{nStudents}. The \code{j}th 
-#' column contains student \code{j}'s ranking over colleges in decreasing order of preference; 
-#' i.e. with '1' being the most preferred. Specifically, cell \code{[i,j]} gives the position of 
-#' college \code{i} in student \code{j}'s preference ranking.
+#' @param s.prefs matrix of dimension \code{nColleges} \code{x} \code{nStudents} with the \code{j}th 
+#' column containing student \code{j}'s ranking over colleges in decreasing order of 
+#' preference (i.e. most preferred first).
 #' @param c.prefs matrix of dimension \code{nStudents} \code{x} \code{nColleges} with the \code{i}th 
-#' column containing college \code{i}'s ranking over students in decreasing order of preference; 
-#' i.e. with '1' being the most preferred. Specifically, cell \code{[j,i]} gives the position of 
-#' student \code{j} in college \code{i}'s preference ranking.
+#' column containing college \code{i}'s ranking over students in decreasing order of 
+#' preference (i.e. most preferred first).
 #' @export
 #' @section Minimum required arguments:
 #' \code{daa} requires the following combination of arguments, subject to the matching problem.
@@ -81,28 +79,28 @@
 daa <- function(nStudents=ncol(s.prefs), nColleges=ncol(c.prefs), nSlots=rep(1,nColleges), s.prefs=NULL, c.prefs=NULL){
 
   ## If 'nColleges' not given, obtain it from nSlots
-  if (is.null(nColleges)){
+  if(is.null(nColleges)){
     nColleges <- length(nSlots)
   }
   ## If no prefs given, make them randomly:
-  if (is.null(s.prefs)){  
+  if(is.null(s.prefs)){  
     s.prefs <- replicate(n=nStudents,sample(seq(from=1,to=nColleges,by=1)))
   }
-  if (is.null(c.prefs)){    
+  if(is.null(c.prefs)){    
     c.prefs <- replicate(n=nColleges,sample(seq(from=1,to=nStudents,by=1)))
   }
   
   ## Consistency checks:
-  if ( dim(s.prefs)[1] != dim(c.prefs)[2] | dim(s.prefs)[2] != dim(c.prefs)[1] | 
+  if( dim(s.prefs)[1] != dim(c.prefs)[2] | dim(s.prefs)[2] != dim(c.prefs)[1] | 
        dim(s.prefs)[2] != nStudents | dim(c.prefs)[2] != nColleges | 
        dim(c.prefs)[1] != nStudents | dim(s.prefs)[1] != nColleges ){
     stop("'s.prefs' and 'c.prefs' must be of dimensions 'nColleges x nStudents' and 'nStudents x nColleges'!")
   }
-  if ( length(nSlots) != nColleges | length(nSlots) != dim(c.prefs)[2] ){
+  if( length(nSlots) != nColleges | length(nSlots) != dim(c.prefs)[2] ){
     stop("length of 'nSlots' must equal 'nColleges' and the number of columns of 'c.prefs'!")
   }
 
-  iter = 0
+  iter <- 0
 
   s.hist    <- rep(0,length=nStudents)  # number of proposals made
   c.hist    <- lapply(nSlots, function(x) rep(0,length=x))  # current students
@@ -110,48 +108,56 @@ daa <- function(nStudents=ncol(s.prefs), nColleges=ncol(c.prefs), nSlots=rep(1,n
 
   s.mat <- matrix(data=1:nStudents,nrow=nStudents,ncol=nColleges,byrow=F)
 
-  while (max(s.hist) < nColleges){  # there are as many rounds as maximal preference orders
+  while(min(s.hist) < nColleges){  # there are as many rounds as maximal preference orders
     # look at market: all unassigned students
     # if history not full (been rejected by all colleges in his prefs)
     # look at unassigned students' history
     # propose to next college on list
-    iter = iter + 1
+    iter         <- iter + 1
+    offers       <- NULL
+    
+    ## Look at unassigned students that have not yet applied to all colleges 
+    temp.singles <- c(na.omit( s.singles[s.hist[s.singles] < nColleges] ))
+    if(length(temp.singles)==0){ # if unassigned students have used up all their offers: stop
+      return(list(s.prefs=s.prefs,c.prefs=c.prefs,iterations=iter-1,matches=c.hist,match.mat=current.match,singles=s.singles))
+      break
+    }
+    
+    ## Add to students' offer history
+    for(i in 1:length(temp.singles)){
+      s.hist[temp.singles[i]] <- s.hist[temp.singles[i]] + 1  # set history of student i one up.
+      offers[i] <- s.prefs[s.hist[temp.singles[i]],temp.singles[i]]  # offer if unassigned i is index of current round college
+    }
+    
     print(paste("Iteration: ",iter))
-    offers <- NULL
-
-    for (i in 1:length(s.singles)){
-      s.hist[s.singles[i]] <- s.hist[s.singles[i]] + 1  # set history of student i one up.
-    offers[i] <- s.prefs[s.hist[s.singles[i]],s.singles[i]]  # offer if unassigned i is index of current round college
-	}
-
-	approached   <- unique(offers)	# index of colleges who received offers
-	temp.singles <- s.singles
-	s.singles    <- NULL	        # reset unassigned students
+    
+	  approached <- unique(offers)	# index of colleges who received offers
+	  s.singles  <- sort(s.singles[!s.singles %in% temp.singles])  # reset unassigned students, except for singles who already used up all offers
 	
-	for (j in approached){
-	  proposers   <- temp.singles[offers==j]
-	  stay.single <- temp.singles[offers==0]		# students who prefer remaining unassigned at current history
+	  for(j in approached){
+	    proposers   <- temp.singles[offers==j]
+	    stay.single <- temp.singles[offers==0]	# students who prefer remaining unassigned at current history
 
-	  for (k in 1:length(proposers)){
-	    if (0 %in% (c.hist[[j]] & any(c.prefs[ ,j]==proposers[k]))){  # if no history and proposer is on preference list
-		  c.hist[[j]][c.hist[[j]]==0][1] <- proposers[k]			  # then accept
-		} else if (TRUE %in% (match(c.prefs[c.prefs[ ,j]==proposers[k],j],c.prefs[ ,j]) < match(c.prefs[c.prefs[ ,j] %in% c.hist[[j]], j],c.prefs[ ,j]))){   # if proposer better than any current student
-		  worst = max(match(c.prefs[c.prefs[ ,j] %in% c.hist[[j]], j], c.prefs[ ,j])) # determine worst current student
-		  s.singles <- c(s.singles,c.prefs[worst,j])   # reject worst current student
-		  c.hist[[j]][c.hist[[j]] == c.prefs[worst,j]] <- proposers[k]	# and take proposer on
-		} else {
-		  s.singles <- c(s.singles,proposers[k])	# otherwise k stays unassigned
-		}
-	  }	
+	    for (k in 1:length(proposers)){
+	      if(0 %in% (c.hist[[j]] & any(c.prefs[ ,j]==proposers[k]))){  # if no history and proposer is on preference list
+		      c.hist[[j]][c.hist[[j]]==0][1] <- proposers[k]			  # then accept
+		    } else if(TRUE %in% (match(c.prefs[c.prefs[ ,j]==proposers[k],j],c.prefs[ ,j]) < match(c.prefs[c.prefs[ ,j] %in% c.hist[[j]], j],c.prefs[ ,j]))){   # if proposer better than any current student
+		      worst <- max(match(c.prefs[c.prefs[ ,j] %in% c.hist[[j]], j], c.prefs[ ,j])) # determine worst current student
+		      s.singles <- c(s.singles,c.prefs[worst,j])   # reject worst current student
+		      c.hist[[j]][c.hist[[j]] == c.prefs[worst,j]] <- proposers[k]	# and take proposer on
+		    } else{
+		      s.singles <- c(s.singles,proposers[k])	# otherwise k stays unassigned
+		    }
+	    }	
     }
 
     s.singles <- sort(c(s.singles,stay.single))
-	if (length(s.singles)==0){	# if no unassigned students left: stop
-	  current.match   <- sapply(1:nColleges, function(x) s.mat[,x] %in% c.hist[[x]])
-	  return(list(s.prefs=s.prefs,c.prefs=c.prefs,iterations=iter,matches=c.hist,match.mat=current.match,singles=s.singles))
+	  if(length(s.singles)==0){	# if no unassigned students left: stop
+	    current.match <- sapply(1:nColleges, function(x) s.mat[,x] %in% c.hist[[x]])
+	    return(list(s.prefs=s.prefs,c.prefs=c.prefs,iterations=iter,matches=c.hist,match.mat=current.match,singles=s.singles))
   	  break
-	}
-	current.match   <- sapply(1:nColleges, function(x) s.mat[,x] %in% c.hist[[x]])
+	  }
+	  current.match <- sapply(1:nColleges, function(x) s.mat[,x] %in% c.hist[[x]])
   }
 
   return(list(s.prefs=s.prefs,c.prefs=c.prefs,iterations=iter,matches=c.hist,match.mat=current.match,singles=s.singles))
